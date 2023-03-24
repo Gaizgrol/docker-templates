b67ed644c601d3b0717a6bf4fbc45d29d7071117
Gaizgrol <gabrielizotongo@gmail.com>
2023-03-24 07:57:15 -0300

    feat: remove angular app and add creation script

--- Angular/docker/entrypoint.sh
+++ Angular/docker/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-FILES=$(ls)
+FILES=$(ls -1)
+FILE_COUNT=$(printf '%b\n' $FILES | wc -l)
 SEARCH_FOR='node_modules'
+
+# No files: Create application from scratch
+if ! [[ "$FILE_COUNT" =~ "0" ]]; then
+  printf 'n' | ng new front-angular --directory . --style scss --routing --skip-git
+fi
 
 # Expose node_modules to host computer to enable IDE intellisense
 if ! [[ "$FILES" =~ "$SEARCH_FOR" ]]; then
